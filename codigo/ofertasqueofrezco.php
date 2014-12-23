8374e7119be16967e1234638eb8eb32b33846d4f
unknown <mitsu.powa@gmail.com>
2014-12-23 23:25:32 +0100

    hmtl que faltaban

--- codigo/ofertasqueofrezco.php
+++ codigo/ofertasqueofrezco.php
@@ -2,8 +2,8 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8"/>
-	<title>Dashboard I Admin Panel</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8_spanish_ci" />
+	<title>Ofertas que ofrezo | Gestor de tiempo</title>
 	
 	<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
 	<!--[if lt IE 9]>
@@ -51,11 +51,18 @@
 
 
 <body>
-
+<?php
+	//Idioma
+	require('language.php'); 
+	$lang = $_GET['lang'];
+	if ( isset($_GET['lang']) ){
+		$lang = $_GET['lang'];
+	}
+?>
 	<header id="header">
 		<hgroup>
-			<h1 class="site_title"><a href="index.php">Panel Usuario</a></h1>
-			<h2 class="section_title">Banco de Horas</h2>
+			<h1 class="site_title"><?php echo __('Deals offered', $lang) ?></h1>
+			<h2 class="section_title"><?php echo __('Time Bank', $lang) ?></h2>
 		</hgroup>
 	</header> <!-- end of header bar -->
 	
@@ -65,34 +72,47 @@
 			<!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
 		</div>
 		<div class="breadcrumbs_container">
-			<article class="breadcrumbs"><a href="index.html">Loggin</a> <div class="breadcrumb_divider"></div> <a class="current">Panel Usuario</a></article>
+			<article class="breadcrumbs">
+			<a href="panel.php?lang=<?php echo $lang; ?>"><?php echo __('Index', $lang) ?></a>
+			<div class="breadcrumb_divider"></div>
+			<a class="current"><?php echo __('Search Panel', $lang) ?></a>
+			<div class="breadcrumb_divider"></div>
+			<a class="current"><?php echo __('Deals offered', $lang) ?></a>
+			</article>
 		</div>
 	</section><!-- end of secondary bar -->
 	
 	<aside id="sidebar" class="column">
 		<form class="quick_search">
-			<input type="text" value="Quick Search" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;">
+			<table>
+				<tr>
+					<td><input type="text" value="" onfocus="if(!this._haschanged){this.value=''};this._haschanged=true;"></td>
+					<td><select style="width:60%;">
+								<option><?php echo __('Selects a category', $lang) ?></option>
+								<?php for($i =0; $i<50; $i++){ ?>
+									 	<option><?php echo __('Category', $lang) ?> <?php echo $i+1 ?></option>
+									<?php } ?> 
+							</select></td>
+				</tr>
+			</table>
+			
+			
+								
+							
 		</form>
 		<hr/>
-		<a href="misofertas.php"><h3>Mis Ofertas</h3></a>
-		<ul class="toggle">
-			<li class="icn_tags"><a href="#">Ejemplo</a></li>
-		</ul>
-		<a href="demanda.php"><h3>Mis Demandas</h3></a>
-		<ul class="toggle">
-			<li class="icn_profile"><a href="#">Ejemplo</a></li>
-		</ul>
-		<a href="valorar.php"><h3>Valorar</h3></a>
-		<ul class="toggle">
-			<li class="icn_video"><a href="#">Ejemplo</a></li>
-		</ul>
-		<a href="notificaciones.php"><h3>Notificaciones</h3></a>
-		<ul class="toggle">
-			<li class="icn_jump_back"><a href="#">Ejemplo</a></li>
-		</ul>
-		<a href="perfil.php"><h3>Perfil</h3></a>
+		<h3><?php echo __('Options', $lang) ?></h3>
+		<ul class="toggle"><!--Opciones-->
 
-		<a href="#"><h3>Salir</h3></a>
+
+			<li class="icn_jump_back"><a href ="javascript:history.back()"><?php echo __('Back', $lang) ?></a></li>
+		
+			<li class="icn_salir"><a href ="salir.php?lang=<?php echo $lang; ?>"><?php echo __('Exit', $lang) ?></a></li>
+
+			<li class="icono_gb"><a href="ofertasqueofrezco.php?lang=en">  Ingles</a></li>
+			<li class="icono_es"><a href="ofertasqueofrezco.php?lang=es">  Castellano</a></li>
+
+		</ul><!--fin opciones-->
 
 		
 		<footer>
@@ -102,27 +122,31 @@
 	</aside><!-- end of sidebar -->
 	
 	<section id="main" class="column">
-		
 		<article class="module width_full">
-			<header><h3>Panel de Usuario</h3></header>
+			<header><h3><?php echo __('Offers', $lang) ?></h3></header>
 				<div class="module_content">
-					<h1>Header 1</h1>
-					<h2>Header 2</h2>
-					<h3>Header 3</h3>
-					<h4>Header 4</h4>
-					<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum. Maecenas faucibus mollis interdum. Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+					<!--tabla-->
+	
+					<div style="display: block;" id="tab1" class="tab_content">
+					<table class="tablesorter" cellspacing="0"> 
+					<tbody> 
+					<?php 
+						for($i =0; $i<5; $i++){ ?>
+						 <tr> 
+				    		<td><?php echo __('Nombre oferta', $lang) ?> <?php echo $i+1?></td> 
+    						<td><input type="checkbox" name="check" ></td> 
+					</tr><?php ;
+					}
+					?> 
+			
+					</tbody> 
+					</table>
+					
+			<!-- fin de la tabla  -->
+			
+</article><!-- end of styles article -->
 
-<p>Donec id elit non mi porta <a href="#">link text</a> gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
 
-					<ul>
-						<li>Donec ullamcorper nulla non metus auctor fringilla. </li>
-						<li>Cras mattis consectetur purus sit amet fermentum.</li>
-						<li>Donec ullamcorper nulla non metus auctor fringilla. </li>
-						<li>Cras mattis consectetur purus sit amet fermentum.</li>
-					</ul>
-				</div>
-		</article><!-- end of styles article -->
-		<div class="spacer"></div>
 	</section>
 
 
